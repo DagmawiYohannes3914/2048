466fccc52e506fc2ef3e7531b42cfbee095dff25
Dagmawi Yohannes <dagmawiyohannes323@gmail.com>
2024-02-20 17:04:41 +0300

    n by n grid

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello")
+def game_matrix(): #this will do the n by n grid
+    global grid_size
+    matrix=[0] * grid_size
+    for row in range(grid_size):
+        matrix[row]=[0] * grid_size
+    return matrix
