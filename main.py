7f347aa8e8245926c25e7f012211cd2504b79483
Dagmawi Yohannes <dagmawiyohannes323@gmail.com>
2024-02-21 16:57:58 +0300

    Adding the display

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ def game_matrix(): #this will do the n by n grid
     for row in range(grid_size):
         matrix[row]=[0] * grid_size
     return matrix
+
+def display_game_matrix(grid):# it will diplay the grid
+    for row in grid:
+        for i in row:
+            print('  ',i,end='  ')
+        print()
